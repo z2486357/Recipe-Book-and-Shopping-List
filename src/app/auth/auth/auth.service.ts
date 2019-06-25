1df6170a1f0d483a242e922f95abd52214311c03
z2486357 <z2486357@gmail.com>
2019-06-24 23:50:53 -0400

    httpclient can't work

--- src/app/auth/auth/auth.service.ts
+++ src/app/auth/auth/auth.service.ts
@@ -1,23 +1,26 @@
 import { Injectable } from '@angular/core';
 import { Http } from "@angular/http";
+import { HttpClient } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
-//interface AuthResponseData {
-//  kind: string;
-//  idToken: string;
-//  email: string;
-//  refreshToken: string;
-//  expiresIn: string;
-//  localId: string;
-//}
+interface AuthResponseData {
+  kind: string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+}
 
 
 @Injectable({providedIn:'root'})
 export class AuthService {
 
-  constructor(private http: Http) { }
+  constructor(private http: HttpClient) { }
 
   signup(email:string,password:string) {
-    return this.http.post(
+    return this.http.post<AuthResponseData>(
       'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyB-4JcEVUbxuvTZyVi3QOe1FJAUbJVDtEA',
       {
         email: email,
