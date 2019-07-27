acf58af841a8e08c142f0c4dea4bdbaf4479b851
z2486357 <z2486357@gmail.com>
2019-07-27 17:31:01 -0400

    Update loading-spinner.component.ts

--- src/app/shared/loadingspinner/loading-spinner.component.ts
+++ src/app/shared/loadingspinner/loading-spinner.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-loading-spinner',
   template: '<div class="lds-hourglass"></div>',
-  styleUrls:['./loading-spinner.component.css']
+  styleUrls: ['./loading-spinner.component.css']
 })
 
 export class LoadingSpinnerComponent { }
