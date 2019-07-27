eee805f36c75fe99d77fb3ebfd212187f184f44a
z2486357 <z2486357@gmail.com>
2019-07-27 17:30:40 -0400

    Update header.component.ts

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,16 +3,16 @@ import { RecipeService } from '../recipes/recipe.service';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 @Component({
-    selector:'app-header',
-    templateUrl:'./header.component.html'
+  selector: 'app-header',
+  templateUrl: './header.component.html'
 })
-export class HeaderComponent{
+export class HeaderComponent {
   constructor(private recipeservice: RecipeService,
-              private shoppinglistservice:ShoppingListService) { }
+    private shoppinglistservice: ShoppingListService) { }
 
   savedata() {
     this.recipeservice.saveToServer().subscribe(
-      (response)=>console.log(response),
+      (response) => console.log(response),
     );
     this.shoppinglistservice.saveToServer().subscribe(
       (response) => console.log(response),
@@ -21,7 +21,7 @@ export class HeaderComponent{
 
   fetchdata() {
     this.recipeservice.fetchFromServer().subscribe(
-      (response)=>console.log(response),
+      (response) => console.log(response),
     )
     this.shoppinglistservice.fetchFromServer().subscribe(
       (response) => console.log(response),
