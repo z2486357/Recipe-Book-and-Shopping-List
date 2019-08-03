8a496a8b21044ade987c4fdd4ab727725640941c
z2486357 <z2486357@gmail.com>
2019-08-03 18:16:35 -0400

    Update recipe.service.ts

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -10,72 +10,72 @@ import { AuthService } from '../auth/auth/auth.service';
 import { HttpParams, HttpClient } from '@angular/common/http';
 
 @Injectable()
-export class RecipeService{
+export class RecipeService {
   newRecipe = new Subject<Recipe[]>();
 
   constructor(private shoppinglistservice: ShoppingListService,
-              private http:HttpClient,
-              private authservice:AuthService) { }
-    private recipes: Recipe[]=[
-        // new Recipe(
-        //     'Recipe 1',
-        //     'This is just a test1',
-        //     'https://farm5.staticflickr.com/4891/45165261135_69cb589907_b.jpg',
-        //     [
-        //         new Ingredient('Meat',1),
-        //         new Ingredient('French Fries',20)
-        //     ]),
-        // new Recipe(
-        //     'Recipe 2',
-        //     'This is just a test2',
-        //     'https://farm5.staticflickr.com/4891/45165261135_69cb589907_b.jpg',
-        //     [
-        //         new Ingredient('Buns',2),
-        //         new Ingredient('Meat',1)
-        //     ]
-        //     )
+    private http: HttpClient,
+    private authservice: AuthService) { }
+  private recipes: Recipe[] = [
+    // new Recipe(
+    //     'Recipe 1',
+    //     'This is just a test1',
+    //     'https://farm5.staticflickr.com/4891/45165261135_69cb589907_b.jpg',
+    //     [
+    //         new Ingredient('Meat',1),
+    //         new Ingredient('French Fries',20)
+    //     ]),
+    // new Recipe(
+    //     'Recipe 2',
+    //     'This is just a test2',
+    //     'https://farm5.staticflickr.com/4891/45165261135_69cb589907_b.jpg',
+    //     [
+    //         new Ingredient('Buns',2),
+    //         new Ingredient('Meat',1)
+    //     ]
+    //     )
   ]
 
   setRecipes(recipes: Recipe[]) {
     this.recipes = recipes;
     this.newRecipe.next(this.recipes.slice());
   }
-    getRecipes(){
-        return this.recipes.slice()
-    }
+  getRecipes() {
+    return this.recipes.slice()
+  }
 
-    getRecipe(index:number){
-        return this.recipes[index]
-    }
+  getRecipe(index: number) {
+    return this.recipes[index]
+  }
 
-    addIngredientToShoppingList(ingredient:Ingredient[]){
-        // for (let ig of ingredient){
-        //     this.shoppinglistservice.addIngredient(ig)
-        // }
-        this.shoppinglistservice.addIngredients(ingredient)
-    }
+  addIngredientToShoppingList(ingredient: Ingredient[]) {
+    // for (let ig of ingredient){
+    //     this.shoppinglistservice.addIngredient(ig)
+    // }
+    this.shoppinglistservice.addIngredients(ingredient)
+  }
 
-    editRecipe(index: number, recipe: Recipe) {
-      this.recipes[index] = recipe;
-      this.newRecipe.next(this.recipes.slice());
-    }
+  editRecipe(index: number, recipe: Recipe) {
+    this.recipes[index] = recipe;
+    this.newRecipe.next(this.recipes.slice());
+  }
 
-    addRecipe(recipe: Recipe) {
-      this.recipes.push(recipe);
-      this.newRecipe.next(this.recipes.slice());
-    }
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.newRecipe.next(this.recipes.slice());
+  }
 
-    deleteRecipe(index:number) {
-      this.recipes.splice(index, 1)
-      this.newRecipe.next(this.recipes.slice());
-    }
+  deleteRecipe(index: number) {
+    this.recipes.splice(index, 1)
+    this.newRecipe.next(this.recipes.slice());
+  }
 
-    saveToServer() {
-      return this.http.put('https://course-project-c59ce.firebaseio.com/recipeData.json', this.getRecipes());
-    }
+  saveToServer() {
+    return this.http.put('https://course-project-c59ce.firebaseio.com/recipeData.json', this.getRecipes());
+  }
 
-    fetchFromServer() {
-      return this.http
+  fetchFromServer() {
+    return this.http
       .get<Recipe[]>(
         'https://course-project-c59ce.firebaseio.com/recipeData.json'
       )
@@ -93,30 +93,30 @@ export class RecipeService{
         })
       );
 
-      //   return this.http.get(
-      //     'https://course-project-c59ce.firebaseio.com/recipeData.json'
-      //     )
-      // .pipe(
-      //   map(
-      //   (response: Response) => {
-      //     if (response.json() === null) {
-      //       console.log('There is no recipe data in server');
-      //     } else {
-      //       this.recipes = response.json();
-      //       for (let recipe of this.recipes) {
-      //         if (!recipe['ingredients']) {
-      //           recipe['ingredients'] = [];
-      //         }
-      //       }
-      //       this.newRecipe.next(this.recipes.slice());  
-      //     }
-          
-      //   }
-      // ),
-      // catchError(
-      //   (error: Response) => {
-      //     return throwError('Something Wrong');
-      //   }
-      // ))
-    }
+    //   return this.http.get(
+    //     'https://course-project-c59ce.firebaseio.com/recipeData.json'
+    //     )
+    // .pipe(
+    //   map(
+    //   (response: Response) => {
+    //     if (response.json() === null) {
+    //       console.log('There is no recipe data in server');
+    //     } else {
+    //       this.recipes = response.json();
+    //       for (let recipe of this.recipes) {
+    //         if (!recipe['ingredients']) {
+    //           recipe['ingredients'] = [];
+    //         }
+    //       }
+    //       this.newRecipe.next(this.recipes.slice());  
+    //     }
+
+    //   }
+    // ),
+    // catchError(
+    //   (error: Response) => {
+    //     return throwError('Something Wrong');
+    //   }
+    // ))
+  }
 }
