7dbbecbf39177c6728eddf09a10d0079ad4b84e4
z2486357 <z2486357@gmail.com>
2019-08-12 16:24:25 -0400

    add authguard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,10 +6,13 @@ import { RecipesDetailComponent } from './recipes/recipes-detail/recipes-detail.
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { AuthComponent } from './auth/auth/auth.component';
+import { AuthGuard } from './auth/auth/auth.guard';
 
 const appRoutes:Routes=[
     {path:'',redirectTo:'/recipes',pathMatch:'full'},
-    {path:'recipes',component:RecipesComponent,children:[
+    {path:'recipes',component:RecipesComponent,
+    canActivate:[AuthGuard],
+    children:[
         {path:'',component:RecipeStartComponent},
         {path:'new',component:RecipeEditComponent},
         {path:':id',component:RecipesDetailComponent},
